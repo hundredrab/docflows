21911e254d2e5760f6ef6b2f82ff398c38105750
hundredrab <sourab.jha@kgec.edu.in>
2018-12-22 19:04:40 +0530

    Add User and Membership models for account.

--- account/models.py
+++ account/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class User(models.Model):
+    """Usernames for platform users.
+
+    Authentication is to be done through LDAP or other plugins.
+    """
+
+    username = models.CharField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    last_accessed = models.DateTimeField(blank=True, null=True)
+
 class Committee(models.Model):
     """Generic committee model.
 
@@ -26,3 +38,15 @@ class Role(models.Model):
 
     class Meta:
         unique_together = ('committee', 'name')
+
+class Member(models.Model):
+    """Members of committees with associated roles."""
+    user = models.ForeignKey(User, related_name='membership')
+    role = models.ForeignKey(Role, related_name='member')
+    added_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username + ' ' + self.role.name
+    
+    class Meta:
+        unique_together = ('user', 'role')
