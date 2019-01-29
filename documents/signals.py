fb39a8546f0a3bc8eaf173107d8efae47c3fb3bb
sarbesh <sarbeshsarkarsks@gmail.com>
2019-01-29 13:07:27 +0530

    signal created in admin

--- documents/signals.py
+++ documents/signals.py
@@ -6,19 +6,6 @@ from .models import Document, Permission
 
 @receiver(post_save, sender=Document)
 def create_permission(sender, **kwargs):
-	# Doc = Document.objects.get(id=kwargs['instance'])
 	if kwargs['created']:
-		per = Permission.objects.create(document=kwargs['instance'], object_id=1, level=1, content_type=4)
-		# per.object_id = 1
-		# per.level = 1
-		# per.content_type = 4 ##for user
-		# per.save(update_fields=['object_id','level','content_type'])
-
-# @receiver(post_save, sender=Document)
-# def save_premissions(sender, **kwargs):
-# 	p = kwargs['instance']
-# 	p.level = 1
-# 	p.content_type = 4 ##4=user
-# 	p.object_id = 1
-# 	p.save()
+		per = Permission.objects.create(document=kwargs['instance'], level=1, holder=kwargs['instance'].owner)
 	
