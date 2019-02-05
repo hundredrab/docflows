b4377208b1804c4a5a0ccbd9b90db1f97c081fb0
nibmgbot <gitbot@nibmg.ac.in>
2019-02-05 18:50:58 +0530

    Add corsheaders settings.

--- docflows/settings/local.py
+++ docflows/settings/local.py
@@ -83,6 +83,15 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3030',
+)
+CORS_ORIGIN_REGEX_WHITELIST = (
+    'localhost:3030',
+)
+
 ROOT_URLCONF = 'docflows.urls'
 
 TEMPLATES = [
